9528bc7416140119970425bfc095891e170eb9e4
Claes Nymand Nilsson <claes89@gmail.com>
2023-06-23 22:49:39 +0200

    changes

---
+++ data/QuizData.js
@@ -0,0 +1,90 @@
+export default data = [
+    {
+        'category': 'Verber',
+        'question': 'Hvilken ordklasse er ordet "begrundelse" i sætningen: Det her kræver vist en god begrundelse',
+        'answers': [
+            'Objekt',
+            'Verbum',
+            'Substantiv',
+            'Subjekt'
+        ],
+        'correct': 'Subjekt',
+    }, {
+        'category': 'Syntaks',
+        'question': 'Hvilken ordklasse tilhører følgende ord: vi, os, dem, sig',
+        'answers': [
+            'Pronominer',
+            'Verber',
+            'Adjektiver',
+            'Dialogiske partikler',
+            'Numeralier',
+            'Adverbier'
+        ],
+        'correct': 'Pronominer'
+    }, {
+        'category': 'Syntaks',
+        'question': 'Hvilket led udgør ordet hvid i følgende sætning: Han malede sin hoveddør hvid hver sommer',
+        'answers': [
+            'Subjektsprædikat',
+            'Objekt',
+            'Verbumprædikat',
+            'Objektsprædikat',
+            'Adled',
+            'Kongruensled',
+        ],
+        'correct': 'Adled',
+    }, {
+        'category': 'Syntaks',
+        'question': 'Hvilken type ledsætning er ledsætningerne i det følgende?',
+        'answers': [
+            'Adverbielle ledsætninger',
+            'Adjektiviske ledsætninger / adled',
+            'Subtantiviske ledsætninger',
+        ],
+        'correct': 'Adjektiviske ledsætninger / adled',
+    },
+    {
+        'category': 'Syntaks',
+        'question': 'Hvor mange artikler er der i sætningen: En dag når vi får en ny bil, kører vi en lang tur til det nordlige Norge',
+        'answers': [
+            '2',
+            '3',
+            '4',
+            '5',
+        ],
+        'correct': '4'
+    },
+    {
+        'category': 'Syntaks',
+        'question': 'Hvilken ordklasse er "at" i sætningen: Husk at lukke for hønsene',
+        'answers': [
+            'Konjuktion',
+            'Artikel',
+            'Adverbium',
+            'Infinitivsmærke',
+        ],
+        'correct': 'Infinitivsmærke',
+    },
+    {
+        'category': 'Syntaks',
+        'question': 'Hvad er subjekt i denne sætning: Logisk er denne opstilling af problemet i hvert fald ikke',
+        'answers': [
+            'Logisk',
+            'denne opstilling',
+            'problemet',
+            'i hvert fald',
+        ],
+        'correct': 'denne opstilling',
+    },
+    {
+        'category': 'Substantiver',
+        'question': 'Hvad er objekt i denne sætning: Hvem er blevet tildelt pengene af firmaet',
+        'answers': [
+            'Hvem',
+            'pengene',
+            'firmaet',
+            'der er ikke noget objekt',
+        ],
+        'correct': 'der er ikke noget objekt'
+    }
+]
